4eedaee136a5a9ad9510885ac4d799f6da74e4a8
mdharnish <147550260+mdharnish@users.noreply.github.com>
2024-09-29 22:39:27 -0500

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -183,11 +183,11 @@ def get_feedback(secret_word, guessed_word):
     for i in range(NUM_LETTERS):
         if guessed_word[i] == secret_word[i]:
             feedback[i] = CORRECT_COLOR
-        elif guessed_word[i] in secret_word[i]:
+        elif guessed_word[i] in secret_word:
             feedback[i] = WRONG_SPOT_COLOR
         else:
             feedback[i] = NOT_IN_WORD_COLOR
-            
+
     # You do not have to change this return statement
     return color_word(feedback, guessed_word)
 
